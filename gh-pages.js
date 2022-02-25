9a02e70726bf3f4b3d098f44ac85e2e2c1e045e4
Juan <juansepulveda.restrepo@gmail.com>
2022-02-25 22:35:44 +0100

    Svelte integration try

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'main',
+        repo: 'https://github.com/jcsepulvedar/jcsepulvedar.github.io.git', // Update to point to your repository  
+        user: {
+            name: 'Juan Sepulveda', // update to use your name
+            email: 'juansepulveda.restrepo@gmail.com' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
